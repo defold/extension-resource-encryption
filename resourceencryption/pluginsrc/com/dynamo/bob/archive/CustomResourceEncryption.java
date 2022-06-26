5d34f15cd640f57a6933a8ba7681bfb8c57dced8
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-06-26 07:36:13 +0200

    Update CustomResourceEncryption.java

--- resourceencryption/pluginsrc/com/dynamo/bob/archive/CustomResourceEncryption.java
+++ resourceencryption/pluginsrc/com/dynamo/bob/archive/CustomResourceEncryption.java
@@ -4,7 +4,7 @@ import com.dynamo.bob.archive.ResourceEncryptionPlugin;
 import com.dynamo.crypt.Crypt;
 
 public class CustomResourceEncryption extends ResourceEncryptionPlugin {
-	private final byte[] KEY = "%/builtins/fonts".getBytes();
+	private final byte[] KEY = "aQj8CScgNP4VsfXK".getBytes();
 
 	@Override
 	public byte[] encrypt(byte[] resource) throws Exception {
